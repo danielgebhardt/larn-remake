345f740211257e9b39b8f9400ee9c69eec09c72b
Daniel Gebhardt <daniel.h.gebhardt.mil@army.mil>
2026-09-03 16:19:26 -0500

    Initializes frontend.

---
+++ frontend/src/__tests/App.test.tsx
@@ -0,0 +1,12 @@
+import {describe, expect, it} from 'vitest'
+import App from "../App.tsx";
+import {render, screen} from "@testing-library/react";
+
+describe('App', () => {
+    it('should render the App component', () => {
+        render(<App/>);
+
+        expect(screen.getByText("Get started")).toBeInTheDocument();
+    });
+})
+

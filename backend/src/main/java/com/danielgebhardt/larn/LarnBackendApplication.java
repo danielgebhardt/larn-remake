b86dcdfcc3847d344fe5f3995f87f2c3a3ece833
Daniel Gebhardt <daniel.h.gebhardt.mil@army.mil>
2026-09-03 15:34:38 -0500

    Adds fresh Spring Initializer backend.

---
+++ backend/src/main/java/com/danielgebhardt/larn/LarnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.danielgebhardt.larn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarnBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielgebhardt/larn/LarnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielgebhardt.larn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
